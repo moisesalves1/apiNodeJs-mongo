7e3cd0035bb87c5f0e3c67372743ac9223ebd69c
moisesalves1 <moisesalves1@gmail.com>
2022-06-14 19:34:34 -0300

    Correção de Bug

--- bin/server.js
+++ bin/server.js
@@ -6,9 +6,9 @@ const debug = require('debug')('nodestr:server');
 
 const port = normalizePort(process.env.port||'3000');
 
-app.set('port',port);
-
 const server = http.createServer(app);
+
+app.set('port', port);
 
 server.listen(port);
 server.on('error', onError);
