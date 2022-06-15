4e0945f3b9059f01df2ffd44a70d14cb18d27a06
moisesalves1 <moisesalves1@gmail.com>
2022-06-15 13:18:05 -0300

    Listando produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,30 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = ((req, res, next) =>{
+    Product.find(
+            {active: true}, 
+            'title price slug'
+        ).then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });;
+});
+
+exports.getBySlug = ((req, res, next) =>{
+    Product.findOne(
+        {
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags'
+        ).then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });;
+});
+
 exports.post = ((req, res, next) => {
     console.log('Entrando no Metodo');
     var product = new Product(req.body);
