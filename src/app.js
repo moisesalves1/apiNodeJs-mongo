c9f71b3f59887368f908a11f1891b081bf4c5028
moisesalves1 <moisesalves1@gmail.com>
2022-06-17 12:34:34 -0300

    Model costumer adicionado

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const router = express.Router();
 mongoose.connect('mongodb+srv://apiNode:mongo123db@cluster0.74guu.mongodb.net/?retryWrites=true&w=majority');
 
 const Product = require('./models/product');
+const Costumer = require('./models/costumer')
 
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
