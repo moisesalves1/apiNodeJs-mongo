88a428bf46ccada624b47b236a03053703539a3b
moisesalves1 <moisesalves1@gmail.com>
2022-06-17 14:56:46 -0300

    Service e-mail | Not Working (sendgrid)

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-9f02-05e78e5761ef'
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem-vindo a Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://apiNode:mongo123db@cluster0.74guu.mongodb.net/?retryWrites=true&w=majority',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -4,6 +4,8 @@ const ValidationContract = require('../validators/fluent-validator');
 const Repository = require('../repositories/customer-repository');
 const md5 = require('md5');
 
+const emailService = require('../services/email-service');
+
 exports.get = async (req, res, next) => {
     try {
     var data = await Repository.get();
@@ -31,7 +33,14 @@ exports.post = async (req, res, next) => {
             name: req.body.name,
             email: req.body.email,
             password: md5(req.body.password + global.SALT_KEY)
-        })
+        });
+
+        // NOT WORKING!
+        // emailService.send(
+        //     req.body.email, 
+        //     'Bem-vindo ao Node Store', 
+        //     global.EMAIL_TMPL.replace('{0}', req.body.name));
+
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso!'
         });
