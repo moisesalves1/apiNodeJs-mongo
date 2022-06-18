2fb7af386a6e19455b75590482ea268747fa3cd3
moisesalves1 <moisesalves1@gmail.com>
2022-06-18 16:16:09 -0300

    Autenticação com Token JWT

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -3,6 +3,7 @@
 const ValidationContract = require('../validators/fluent-validator');
 const Repository = require('../repositories/customer-repository');
 const md5 = require('md5');
+const authService = require('../services/auth-service');
 
 const emailService = require('../services/email-service');
 
@@ -43,6 +44,38 @@ exports.post = async (req, res, next) => {
 
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso!'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: "Falha ao processar sua requisição"
+        });
+    } 
+};
+
+exports.authenticate = async (req, res, next) => {
+    try {
+        const customer = await Repository.authenticate({
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        });
+
+        if (!customer) {
+            res.status(400).send({
+                message: 'Usuário ou senha inválidos'
+            });
+                return;
+        }
+
+        const token = await authService.generateToken({
+            email: customer.email,
+            name: customer.name
+        })
+        res.status(201).send({
+            token: token,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
         });
     } catch (e) {
         res.status(500).send({
