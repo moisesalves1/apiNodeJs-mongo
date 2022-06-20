b59552a39a7a8016b81a355ac9ae1b017ee49f88
Moises Alves <87319078+moisesalves1@users.noreply.github.com>
2022-06-20 10:45:32 -0300

    delete config.js
    dados sensíveis

--- src/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-9f02-05e78e5761ef'
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem-vindo à Node Store!';
-
-module.exports = {
-    connectionString: 'mongodb+srv://apiNode:mongo123db@cluster0.74guu.mongodb.net/?retryWrites=true&w=majority',
-    sendgridKey: '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',
-    userImagesBlobConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodeapi;AccountKey=rosLhIbGrmWpYppD9Krm2Gf532IeZNIrFe66Kx9uJL1DeAOJld6WtlTUVfbSJ08qNKVW2MrZqP8y+AStqEWu8A==;EndpointSuffix=core.windows.net'
-}
